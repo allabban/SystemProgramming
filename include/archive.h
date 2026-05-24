b362038a02504355ca2d53839cf35e11f39f0d12
DarkAndLighto <ammarajam08@gmail.com>
2026-05-24 21:19:38 +0300

    Added Rapor and fixed errors

--- include/archive.h
+++ include/archive.h
@@ -1,6 +1,7 @@
 #ifndef ARCHIVE_H
 #define ARCHIVE_H
 
+// Archives multiple input files into a single output file
 int archive_files(char *input_files[], int file_count, const char *output_filename);
 
 #endif
--- include/extract.h
+++ include/extract.h
@@ -1,6 +1,7 @@
 #ifndef EXTRACT_H
 #define EXTRACT_H
 
+// Extracts an archive file into the target directory
 int extract_archive(const char *archive_filename, const char *target_directory);
 
 #endif
